58ace8ca97796b8146edc7c18bd901277fcd25cd
gabox <gaboxin@gmail.com>
2019-01-24 11:00:07 -0600

    v2.0.12 - msi-11 ok Parametrizado y sin directorios up/down loads

--- sube.bat
+++ sube.bat
@@ -1,5 +1,5 @@
 git status
 git add .
-git commit -m "v2.0.11 - msi-11 ok Parametrizado "
+git commit -m "v2.0.12 - msi-11 ok Parametrizado y sin directorios up/down loads"
 git push origin master
 git push heroku master
